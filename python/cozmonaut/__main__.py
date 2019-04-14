3774c074bc3cf8a89ed8b53afa0bbf9688683241
Tyler Filla <tylerfilla@hotmail.com>
2019-04-14 02:04:01 -0500

    Add guard against direct use of python

---
+++ python/cozmonaut/__main__.py
@@ -0,0 +1,8 @@
+#
+# Cozmonaut
+# Copyright 2019 The Cozmonaut Contributors
+#
+
+if __name__ == '__main__':
+    print('This program cannot be run standalone.')
+    print('Please use the entry executable or a test harness.')
